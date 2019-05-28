083db8bcf68e61ecfbee35812b9508869abd4808
amrutha2129 <s.vas.amrutha@gmail.com>
2019-05-28 18:08:49 +1000

    Code cleanup and created Slider.jsx file for slideshow

---
+++ ImageSlider/ClientApp/src/components/Slider.jsx
@@ -0,0 +1,12 @@
+﻿import React, { Component } from 'react';
+
+export class Slider extends Component {
+   
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+            </div>
+        );
+    }
+}
